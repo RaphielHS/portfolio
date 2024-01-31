2ff4c9334ac5fcef5785d9f6e963d61afab5d010
RaphielHS <70839561+RaphielHS@users.noreply.github.com>
2024-01-31 19:13:33 +0700

    Add files via upload

---
+++ build/precache-manifest.fd665035f259e9d6ac391076f267249e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "524e4dc47c20518649363a4cb5a31786",
+    "url": "/portofolio/index.html"
+  },
+  {
+    "revision": "c8dec1f57fabb20e0098",
+    "url": "/portofolio/static/css/main.2e0e63d8.chunk.css"
+  },
+  {
+    "revision": "c77a1dc29a58453f470f",
+    "url": "/portofolio/static/js/2.c3861c8e.chunk.js"
+  },
+  {
+    "revision": "c8dec1f57fabb20e0098",
+    "url": "/portofolio/static/js/main.bb2c5065.chunk.js"
+  },
+  {
+    "revision": "8ebee44fab047a9cd28e",
+    "url": "/portofolio/static/js/runtime~main.64b239cf.js"
+  }
+]);
